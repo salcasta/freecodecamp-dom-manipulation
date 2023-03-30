db92aaed652a1c0cc294e5a0986b81b4c4af4aa7
salcasta <salcasta@gmail.com>
2023-03-29 22:52:13 -0500

    add event listeners

--- app.js
+++ app.js
@@ -112,3 +112,87 @@
 // REMOVE ELEMENTS
 
 // li.remove();
+
+
+
+//TRAVERSE THE DOM
+
+
+//PARENT NODE TRAVERSAL
+
+// let ul = document.querySelector('ul');
+
+// console.log(ul.parentNode)
+
+// OR 
+
+// console.log(ul.parentElement)
+
+
+// to find grandparent 
+
+// console.log(ul.parentNode.parentNode)
+
+// OR
+
+// console.log(ul.parentElement.parentNode)
+
+
+// CHILD Node Traveral
+
+// let ul = document.querySelector('ul');
+
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+
+
+// SIBLING NODE TRAVERSAL
+
+// let ul = document.querySelector('ul');
+
+// console.log(ul.previousElementSibling);
+// console.log(ul.nextElementSibling);
+
+
+
+// DOM EVENT LISTENERS
+
+// element.addEventListener('click', function);
+
+
+// const buttonTwo = document.querySelector('.btn-2');
+
+// function alertBtn() {
+//     alert('I also love JavaScript');
+// }
+
+// buttonTwo.addEventListener('click', alertBtn);
+
+
+// MOUSEOVER
+
+// const newBackgroundColor = document.querySelector('.box-3');
+
+// function changeBgColor() {
+//     newBackgroundColor.style.backgroundColor = 'blue';
+// }
+
+// newBackgroundColor.addEventListener('mouseover', changeBgColor);
+
+
+
+//REVEAL EVENT
+
+// const revealBtn = document.querySelector('.reveal-btn');
+// const hiddenContent = document.querySelector('.hidden-content');
+
+// function revealContent() {
+//     if (hiddenContent.classList.contains('reveal-btn')) {
+//         hiddenContent.classList.remove('reveal-btn')
+//     } else {
+//         hiddenContent.classList.add('reveal-btn')
+//     }
+// }
+
+// revealBtn.addEventListener('click', revealContent);
